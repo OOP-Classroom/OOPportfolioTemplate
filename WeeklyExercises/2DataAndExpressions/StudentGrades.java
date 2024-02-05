10f2ed7029b178016b14b27afa806535c9bd81bf
Zach <98361344+ZachBaig@users.noreply.github.com>
2024-02-05 15:41:03 +0000

    Create StudentGrades.java

---
+++ WeeklyExercises/2DataAndExpressions/StudentGrades.java
@@ -0,0 +1,18 @@
+public class StudentGrades {
+
+	public static void main(String[] args) 
+	{
+		System.out.println("///////////////////\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\r\n"
+				+ "==          Student Points          ==\r\n"
+				+ "\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\///////////////////r\n");
+		
+		System.out.println("Name            Lab     Bonus   Total\r\n"
+				+ "----            ---     -----   -----");
+		System.out.println("Bartholomew     75      5        80");
+		System.out.println("Shanequa        54      2        56");
+		System.out.println("Aladdin         34      10       44");
+		System.out.println("Jasmine         10      1        11");
+		System.out.println("Ben Tennyson    99      1        100");
+	}
+
+}
